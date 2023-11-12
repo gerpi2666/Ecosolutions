94364a9792e1af6fbd6096ca9d041981a55e607d
gerpi <gerpi.2666@gmail.com>
2023-11-12 01:01:55 -0600

    Fix material error

---
+++ src/app/material/index/index.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+
+
+
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+
+
+export class IndexComponent {
+
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card'; 
 
 import { MaterialRoutingModule } from './material-routing.module';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
     CommonModule,
-    MaterialRoutingModule
+    MaterialRoutingModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
