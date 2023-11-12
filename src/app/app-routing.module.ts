4a5d9478042c32c01585aa63951c4315c3286758
gerpi <gerpi.2666@gmail.com>
2023-11-12 17:30:00 -0600

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IndexComponent} from 'src/app/material/index/index.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'material', component:IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
