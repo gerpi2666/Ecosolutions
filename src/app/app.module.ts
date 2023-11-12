b2a7af31f062042657993fe8f420ed4e29208e39
gerpi <gerpi.2666@gmail.com>
2023-11-12 00:36:43 -0600

    firt

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { MaterialModule } from './material/material.module';
+import { CenterModule } from './center/center.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    MaterialModule,
+    CenterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/center/center.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CenterRoutingModule } from './center-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CenterRoutingModule
+  ]
+})
+export class CenterModule { }
