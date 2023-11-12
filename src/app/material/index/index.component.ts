6ebd835b7801487545888cbea644d91c03eb19ab
geraldSinertica <106938284+LauOG05@users.noreply.github.com>
2023-11-12 15:59:33 -0600

    material index

--- src/app/material/index/index.component.ts
+++ src/app/material/index/index.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Subject, takeUntil } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { GenericService } from 'src/app/share/generic.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 
@@ -13,5 +17,42 @@ import { MatCardModule } from '@angular/material/card';
 
 
 export class IndexComponent {
+  datos:any; //Respuesta del API
+  filterDatos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+
+  dataSource = new MatTableDataSource<any>();
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['nombre', 'precio','acciones'];
+
+  constructor(private gService: GenericService, 
+    private router: Router,
+    private route: ActivatedRoute)
+  {
+    this.listarMaterial();
+
+  }
+
+
+  listarMaterial(){
+    //Solicitud al API para listar todos los videojuegos
+    //localhost:3000/videojuego
+    this.gService.list('material/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response:any)=>{
+        console.log(response);
+        this.datos=response;
+        this.filterDatos=this.datos
+      })
+  }
+
+  
+  ngOnDestroy(){
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
 
 }
