0eff5cf2f9172b3ccd5cf320716d8831315c66d0
gerpi <gerpi.2666@gmail.com>
2023-11-12 19:43:16 -0600

    fix core aplications

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { CoreRoutingModule } from './core-routing.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
-    PageNotFoundComponent
+    SidebarComponent
   ],
   imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent
+    CommonModule,
+    CoreRoutingModule
   ]
 })
 export class CoreModule { }
