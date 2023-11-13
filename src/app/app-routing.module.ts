04bad4714bfdf090d23e5eb98d218ea036d803f8
gerpi <gerpi.2666@gmail.com>
2023-11-12 23:51:12 -0600

    add side bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IndexComponent} from 'src/app/material/index/index.component'
+import { TemplateComponent } from './core/template/template.component';
 
 const routes: Routes = [
-  {path: 'material', component:IndexComponent }
+ 
+  {
+    path: '',
+    component: TemplateComponent
+    ,
+    children: [
+      {
+        path: 'material',
+        component: IndexComponent,
+      },
+     
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,66 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+
+
+//
+import {MatInputModule} from "@angular/material/input"
+import {MatSelectModule} from "@angular/material/select"
+import {MatAutocompleteModule} from "@angular/material/autocomplete"
+import {MatToolbarModule} from "@angular/material/toolbar"
+import {MatMenuModule} from "@angular/material/menu"
+import {MatIconModule} from "@angular/material/icon"
+import {MatButtonModule} from "@angular/material/button"
+import {MatBadgeModule} from "@angular/material/badge"
+import {MatSidenavModule} from "@angular/material/sidenav"
+import {MatListModule} from "@angular/material/list"
+import {MatCardModule} from "@angular/material/card"
+import {MatSliderModule} from "@angular/material/slider"
+import {MatTableModule} from "@angular/material/table"
+import {MatPaginatorModule} from "@angular/material/paginator"
+import {MatSortModule} from "@angular/material/sort"
+import {MatDatepickerModule} from "@angular/material/datepicker"
+import {MatNativeDateModule} from "@angular/material/core"
+import {MatRadioModule} from "@angular/material/radio"
+import {MatCheckboxModule} from "@angular/material/checkbox"
+import {MatDialogModule} from "@angular/material/dialog";
+import { TemplateComponent } from './template/template.component';
+import { FooterComponent } from './footer/footer.component'
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    TemplateComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDialogModule
+  ],exports:[
+    TemplateComponent
   ]
 })
 export class CoreModule { }
