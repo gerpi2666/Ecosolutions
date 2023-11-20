e3f342fe0f17e8d9c1b42b7f85ba273de116b1cb
gerpi <gerpi.2666@gmail.com>
2023-11-19 23:18:13 -0600

    fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {IndexComponent} from 'src/app/material/index/index.component'
-import {IndexCenterComponent} from './center/index/index.component'
-
-
-
-import { TemplateComponent } from './core/template/template.component';
-import { IndexOrdenComponent } from './orden/index-orden/index-orden.component';
-import { DetailComponent } from './orden/detail/detail.component';
-import { OrdenCenterComponent } from './orden/orden-center/orden-center.component';
-import { CreateComponent } from './material/create/create.component';
-import { CreateCenterComponent } from './center/create/create.component';
-import { LandingModule } from './landing/landing.module';
 import { HomeLandingComponent } from './landing/home-landing/home-landing.component';
 
 const routes: Routes = [
